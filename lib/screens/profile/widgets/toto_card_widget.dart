b539aa96c71297eb938bd44bf7f82aa0baecf44d
YearaChoi <22200750@handong.ac.kr>
2024-11-28 00:23:28 +0900

    feat: 프로필 페이지 수정, 삭제 메뉴 클릭시 alert dialog 작업

--- lib/screens/profile/widgets/toto_card_widget.dart
+++ lib/screens/profile/widgets/toto_card_widget.dart
@@ -25,45 +25,6 @@ class ToToCard extends StatefulWidget {
 class _ToToCardState extends State<ToToCard> {
   bool isLiked = false;
 
-  void _showOptionsModal() {
-    showModalBottomSheet(
-      context: context,
-      backgroundColor: Colors.transparent,
-      builder: (BuildContext context) {
-        return Container(
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-          ),
-          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ListTile(
-                leading: const Icon(Icons.edit, color: Colors.black),
-                title: const Text(
-                  '투투 수정',
-                  style: TextStyle(fontSize: 16),
-                ),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.delete, color: Colors.black),
-                title: const Text('투투 삭제',
-                    style: TextStyle(fontSize: 16, color: Colors.black)),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -149,5 +110,104 @@ class _ToToCardState extends State<ToToCard> {
         .animate()
         .fadeIn(duration: 1000.ms)
         .slideY(begin: 0.1, end: 0, duration: 1000.ms);
+  }
+
+  void _showOptionsModal() {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext context) {
+        return Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: const Icon(Icons.edit, color: Colors.black),
+                title: const Text(
+                  '투투 수정',
+                  style: TextStyle(fontSize: 16),
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                  _showConfirmationDialog('edit');
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.delete, color: Colors.black),
+                title: const Text('투투 삭제',
+                    style: TextStyle(fontSize: 16, color: Colors.black)),
+                onTap: () {
+                  Navigator.pop(context);
+                  _showConfirmationDialog('delete');
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _showConfirmationDialog(String action) {
+    String title = action == 'edit' ? '투투 수정' : '투투 삭제';
+    String content = action == 'edit'
+        ? '투투를 수정하시겠습니까?\n티켓 포인트 ***p가 소모됩니다.'
+        : '투투를 삭제하시겠습니까?\n이 작업은 되돌릴 수 없습니다.';
+    String confirmText = action == 'edit' ? '수정' : '삭제';
+    Color confirmColor = action == 'edit' ? Colors.blue : Colors.red;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            title,
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+          ),
+          content: Text(content),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('취소',
+                  style: TextStyle(color: Color.fromARGB(255, 133, 133, 133))),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+                if (action == 'edit') {
+                  _performEditAction();
+                } else if (action == 'delete') {
+                  _performDeleteAction();
+                }
+              },
+              child: Text(confirmText, style: TextStyle(color: confirmColor)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _performEditAction() {
+    // 수정 동작 로직 추가
+  }
+
+  void _performDeleteAction() {
+    // 삭제 동작 로직 추가
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('투투가 삭제되었습니다.'),
+        duration: Duration(seconds: 1),
+        behavior: SnackBarBehavior.fixed,
+      ),
+    );
   }
 }
